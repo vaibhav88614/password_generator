51663c671e51e3a3f81276ad237096b9af0bcbf6
Vaibhav Patil <59771830+vaibhav88614@users.noreply.github.com>
2024-06-30 23:17:20 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,3 +3,7 @@ import random
 # Generate a random number between 0 and 100
 random_number = random.randint(0, 100)
 print(random_number)
+
+# Generate a random character from a-z
+random_char = random.choice('abcdefghijklmnopqrstuvwxyz')
+print(random_char)
